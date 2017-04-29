aa685e4563a578a1a3b0dcfc5c35aca2fe3ccd19
514700 <birenjaspal@C1MRQ144HF4H.local>
2017-04-29 15:01:53 -0400

    framework started

---
+++ controllers/pta.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var bcrypt = require('bcrypt');
+var Users = require('../models/users.js');
+
+//insert routes here
+
+module.exports = router;
---
+++ controllers/users.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var bcrypt = require('bcrypt');
+
+var User = require('../models/users.js');
+
+router.get('/new', function(req, res){
+    res.render('users/new.ejs');
+});
+
+router.post('/', function(req, res){
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    User.create(req.body, function(err, createdUser){
+        res.redirect('/');
+    });
+});
+
+module.exports = router;
