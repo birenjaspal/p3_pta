b964b2a3df6ff14995395b8a338e34f562ba0146
514700 <birenjaspal@C1MRQ144HF4H.local>
2017-05-02 11:29:31 -0400

    landing page added

--- controllers/pta.js
+++ controllers/pta.js
@@ -7,14 +7,14 @@ var Events = require('../models/events.js');
 //insert routes here
 
 //GET ROUTE=================================================================================
-router.get('/', function(req, res){
+router.get('/events/index.html', function(req, res){
     Events.find({}, function(err, foundEvents){
         res.json(foundEvents);
     });
 });
 
 //POST ROUTE=================================================================================
-router.post('/', function(req, res){
+router.post('/events/', function(req, res){
     Events.create(req.body, function(err, createdEvent){
         res.json(createdEvent);
     });
@@ -22,7 +22,7 @@ router.post('/', function(req, res){
 
 
 //DELETE ROUTE ==============================================================================
-router.delete('/:id', function(req, res){
+router.delete('/events/:id', function(req, res){
     Events.findByIdAndRemove(req.params.id, function(err, deletedEvent){
         res.json(deletedEvent);
     });
@@ -30,7 +30,7 @@ router.delete('/:id', function(req, res){
 
 
 //UPDATE ROUTE================================================================================
-router.put('/:id', function(req, res){
+router.put('/events/:id', function(req, res){
     Events.findByIdAndUpdate(req.params.id, req.body, {new:true}, function(err, updatedEvent){
         res.json(updatedEvent);
     });
