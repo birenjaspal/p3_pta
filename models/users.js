32b0db3f4070e3a3799015c00115a254299296e9
514700 <birenjaspal@C1MRQ144HF4H.local>
2017-05-03 16:52:20 -0400

    authentication started

--- models/users.js
+++ models/users.js
@@ -1,19 +1,26 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+
 var userSchema = Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  hash: String,
-  salt: String
+    username: String,
+    password: String
 });
+
+//---uncomment the below for json token authentication---
+// var userSchema = Schema({
+//   email: {
+//     type: String,
+//     unique: true,
+//     required: true
+//   },
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   hash: String,
+//   salt: String
+// });
 
 var User = mongoose.model('User', userSchema);
 
