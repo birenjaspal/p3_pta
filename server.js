fd97dbaaab282e0c906c9f9ede1aabba79fda857
514700 <birenjaspal@C1MRQ144HF4H.local>
2017-05-03 07:29:43 -0400

    clean up

--- server.js
+++ server.js
@@ -14,18 +14,8 @@ app.use('/seed', seedController);
 
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(session({
-    secret: "feedmeseymour", //some random string
-    resave: false,
-    saveUninitialized: false
-}));
+
 app.use(express.static("public"));
-
-var usersController = require('./controllers/users.js');
-app.use('/users', usersController);
-
-var sessionsController = require('./controllers/sessions.js');
-app.use('/sessions', sessionsController);
 
 var ptaController = require('./controllers/pta.js');
 app.use('/pta', ptaController);
@@ -35,19 +25,7 @@ app.use('/events', ptaController);
 
 
 
-app.get('/', function(req, res){
-    res.render('index.ejs', {
-        currentUser: req.session.currentuser
-    });
-});
 
-app.get('/app', function(req, res){
-    if(req.session.currentuser){
-        res.send('the party');
-    } else {
-        res.redirect('/sessions/new');
-    }
-});
 
 mongoose.connect(mongoUri);
 
